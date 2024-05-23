f2e853190f23cb00a4a422fd641834b805338803
gabs <gabriel.micael@sou.unifal-mg.edu.br>
2024-05-23 17:25:30 -0300

    feat: animation in/out input

--- InSearchOn(front)/src/public/api.js
+++ InSearchOn(front)/src/public/api.js
@@ -1,5 +1,6 @@
 var inputPesquisa = document.getElementById("input-pesquisa");
 const resultSearch = document.getElementById("result");
+var from = 1
 
 inputPesquisa.addEventListener("keypress", function (event) {
     // Verifica se a tecla pressionada é a tecla "Enter" (código 13)
@@ -43,14 +44,52 @@ function formatJSON(jsonData) {
     return jsonData.map(item => {
         return `
                     <div>
-                        <h2>${item.title}</h2>
-                        <a href="${item.url}" class="url" style="font-size: 12px;">${item.url}</a>
+                        <div style="max-width:250px;">
+                            <a href="${item.url}">
+                                <h2>${item.title}</h2>
+                                <p class="url" style="font-size: 12px;">${item.url}</p>
+                            <a>
+                        </div>
                         <br>
                         <p class="abs" style="font-size: 16px;">${item.abs}</p>
                     </div>
                 `;
+        // `
+        // <table>
+        //     <tr>
+        //         <th> ${item.title} </th>
+        //     </tr>
+        //     <tr>
+        //         <td>
+        //             <a href="${item.url}" class="url" style="font-size: 12px;">${item.url}</a>
+        //         </td>
+        //     </tr>
+        //     <tr>
+        //         <p class="abs" style="font-size: 16px;">${item.abs}</p>
+        //     </tr>
+        // </table>
+        // `;
     }).join(''); // Precisa juntar todos os elementos do array em uma string
 }
 
+document.addEventListener('DOMContentLoaded', function() {
+    var result = document.getElementById('result');
+  
+    // Função para verificar se há conteúdo
+    function checkContent() {
+      if (result.innerHTML.trim() === '') {
+        result.classList.add('empty');
+      } else {
+        result.classList.remove('empty');
+      }
+    }
+  
+    // Verificar conteúdo ao carregar a página
+    checkContent();
+  
+    // Verificar conteúdo sempre que for modificado
+    result.addEventListener('DOMSubtreeModified', checkContent);
+  });
+  
 
 

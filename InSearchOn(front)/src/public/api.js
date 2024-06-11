cdec3c5a7c393c321ed09a2b568c365677a9f4cb
gabs <gabriel.micael@sou.unifal-mg.edu.br>
2024-06-11 12:06:04 -0300

    feat: add a initial pagination

--- InSearchOn(front)/src/public/api.js
+++ InSearchOn(front)/src/public/api.js
@@ -1,6 +1,9 @@
 var inputPesquisa = document.getElementById("input-pesquisa");
 const resultSearch = document.getElementById("result");
-var from = 1
+var from = 0
+const tamPag = 10
+const qtdResultados = 50;
+var restultados;
 
 inputPesquisa.addEventListener("keypress", function (event) {
     // Verifica se a tecla pressionada é a tecla "Enter" (código 13)
@@ -13,7 +16,6 @@ inputPesquisa.addEventListener("keypress", function (event) {
 // Função para realizar a busca
 function busca() {
     console.log("entrou");
-
     const valorInput = inputPesquisa.value;
     fetch("http://localhost:8080/v1/search?query=" + valorInput, {
         method: "GET",
@@ -27,6 +29,7 @@ function busca() {
         .then(function (jsonData) {
             const formattedData = formatJSON(jsonData);
             resultSearch.innerHTML = formattedData;
+            resultados = jsonData;
             // Verifica se a div está vazia
             if (resultSearch.innerHTML.trim() === "") {
                 resultSearch.style.display = "none"; // Se estiver vazia, oculta a div
@@ -41,7 +44,7 @@ function busca() {
 
 // Função para formatar os dados JSON
 function formatJSON(jsonData) {
-    return jsonData.slice(0,10).map(item => {
+    return jsonData.slice(from,from+tamPag).map(item => {
         return `
                     <tr>
                         <td style="max-width:700px;">
@@ -55,30 +58,23 @@ function formatJSON(jsonData) {
                              <p class="url" style="font-size: 12px;">${item.url}</p>
                         <a>
                     </tr>
-                           <p class="abs" style="font-size: 16px;">${item.abs}</p>
+                           <p class="abs" style="font-size: 16px;">${item.abs}...</p>
                         </td>
                     </tr>
                 `;
     }).join(''); // Precisa juntar todos os elementos do array em uma string
 }
 
+
 document.addEventListener('DOMContentLoaded', function() {
+    var test = document.getElementById('test');
     var result = document.getElementById('result');
-  
     // Função para verificar se há conteúdo
     function checkContent() {
       if (result.innerHTML.trim() === '') {
-        if (!result.classList.contains('empty')) {
-          result.classList.add('empty');
-          result.style.padding = '0';
-          result.style.maxHeight = '0';
-        }
+          test.style.display = 'none';
       } else {
-        if (result.classList.contains('empty')) {
-          result.classList.remove('empty');
-          result.style.padding = '10px 15px 15px 15px';
-          result.style.maxHeight = 'none'; // ou qualquer outra altura desejada
-        }
+          test.style.display = 'block'; // ou qualquer outra altura desejada
       }
     }
   
@@ -90,6 +86,34 @@ document.addEventListener('DOMContentLoaded', function() {
     observer.observe(result, { childList: true, subtree: true });
   });
   
+  function proxPage(){
+    var botaoProx = document.getElementById('avancar'); 
+
+    if(from + tamPag < 60){
+        from = from + tamPag;
+        if(from >= qtdResultados){
+            botaoProx.setAttribute('disabled',true);
+        }else{
+            botaoProx.setAttribute('disabled',false);
+            result.innerHTML = formatJSON(resultados);
+        } 
+    }
+    
+  }
+
+  function antPage(){
+    var botaoAnt = document.getElementById('voltar'); 
+
+    if(from - tamPag >= 0){
+        from = from - tamPag;
+        if(from = 0){
+            botaoAnt.setAttribute('disabled',true);
+        }else{
+            botaoAnt.setAttribute('disabled',false);
+            result.innerHTML = formatJSON(resultados);
+        }
+    }
+  }
 
   
   
